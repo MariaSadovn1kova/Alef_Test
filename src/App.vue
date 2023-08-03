2b1808d8cf3dd731815205e9a9bf34e725baa7f9
Maria <msadovnikov904@gmail.com>
2023-08-03 13:57:27 +0800

    Project Initialization

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>Тут будет навбарчик родненький</header>
+
+  <RouterView />
+</template>
